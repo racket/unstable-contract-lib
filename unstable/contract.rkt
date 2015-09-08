36f4e320c574cef6630ec34ac73f69a2911b86b0
Vincent St-Amour <stamourv@racket-lang.org>
2015-09-08 17:12:00 -0500

    Move `rename-contract`, `if/c` and `failure-result/c` to `racket/contract`.

--- unstable/contract.rkt
+++ unstable/contract.rkt
@@ -8,66 +8,6 @@
 (define path-piece?
   (or/c path-string? (symbols 'up 'same)))
 
-;; ryanc added:
-
-;; (if/c predicate then/c else/c) applies then/c to satisfying
-;;   predicate, else/c to those that don't.
-(define (if/c predicate then/c else/c)
-  #|
-  Naive version:
-    (or/c (and/c predicate then/c)
-          (and/c (not/c predicate) else/c))
-  But that applies predicate twice.
-  |#
-  (let ([then-ctc (coerce-contract 'if/c then/c)]
-        [else-ctc (coerce-contract 'if/c else/c)])
-    (define name (build-compound-type-name 'if/c predicate then-ctc else-ctc))
-    ;; Special case: if both flat contracts, make a flat contract.
-    (if (and (flat-contract? then-ctc)
-             (flat-contract? else-ctc))
-        ;; flat contract
-        (let ([then-pred (flat-contract-predicate then-ctc)]
-              [else-pred (flat-contract-predicate else-ctc)])
-          (define (pred x)
-            (if (predicate x) (then-pred x) (else-pred x)))
-          (flat-named-contract name pred))
-        ;; ho contract
-        (let ([then-proj (contract-projection then-ctc)]
-              [then-fo (contract-first-order then-ctc)]
-              [else-proj (contract-projection else-ctc)]
-              [else-fo (contract-first-order else-ctc)])
-          (define ((proj blame) x)
-            (if (predicate x)
-                ((then-proj blame) x)
-                ((else-proj blame) x)))
-          (make-contract
-           #:name name
-           #:projection proj
-           #:first-order
-           (lambda (x) (if (predicate x) (then-fo x) (else-fo x))))))))
-
-;; failure-result/c : contract
-;; Describes the optional failure argument passed to hash-ref, for example.
-;; If the argument is a procedure, it must be a thunk, and it is applied. Otherwise
-;; the argument is simply the value to return.
-(define failure-result/c
-  (if/c procedure? (-> any) any/c))
-
-;; rename-contract : contract any/c -> contract
-;; If the argument is a flat contract, so is the result.
-(define (rename-contract ctc name)
-  (let ([ctc (coerce-contract 'rename-contract ctc)])
-    (if (flat-contract? ctc)
-        (flat-named-contract name (flat-contract-predicate ctc))
-        (let* ([make-contract (if (chaperone-contract? ctc) make-chaperone-contract make-contract)])
-          (define (stronger? this other)
-            (contract-stronger? ctc other))
-          (make-contract #:name name
-                         #:projection (contract-projection ctc)
-                         #:first-order (contract-first-order ctc)
-                         #:stronger stronger?
-                         #:list-contract? (list-contract? ctc))))))
-
 ;; Added by asumu
 ;; option/c : contract -> contract
 (define (option/c ctc-arg)
@@ -143,9 +83,6 @@
 (provide/contract
  [path-piece? contract?]
 
- [if/c (-> procedure? contract? contract? contract?)]
- [failure-result/c contract?]
- [rename-contract (-> contract? any/c contract?)]
  [rename option/c maybe/c (-> contract? contract?)]
 
  [truth/c flat-contract?]
@@ -154,4 +91,7 @@
 (provide sequence/c
          non-empty-string?
          port-number?
-         (rename-out [listen-port-number? tcp-listen-port?]))
+         (rename-out [listen-port-number? tcp-listen-port?])
+         rename-contract
+         if/c
+         failure-result/c)
